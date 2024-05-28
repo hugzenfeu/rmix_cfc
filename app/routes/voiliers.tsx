9bd3311dd615d6c480bfd1e64ef6e6493f61e84c
Hugo Casanave <casanave.hugo@gmail.com>
2024-05-28 19:16:21 +0200

    cleanup du nom des variables

--- app/routes/voiliers.tsx
+++ app/routes/voiliers.tsx
@@ -3,18 +3,20 @@ import Voilier from "~/components/containers/Voilier";
 
 export default function voiliers() {
   return (
-    <div className="flex flex-wrap">
-      <Voilier />
-      <Voilier />
-      <Voilier />
-      <Voilier />
-      <Voilier />
-      <Voilier />
+    <div>
+      <div className="flex flex-wrap">
+        <Voilier />
+        <Voilier />
+        <Voilier />
+        <Voilier />
+        <Voilier />
+        <Voilier />
 
-      <Voilier />
-      <Voilier />
-      <Voilier />
-      <Voilier />
+        <Voilier />
+        <Voilier />
+        <Voilier />
+        <Voilier />
+      </div>
     </div>
   );
 }
